3bce23cf56f91419d519a4eda5594a80bedcfe2f
jonbowring <jonbowring@gmail.com>
2015-10-01 22:18:18 +0200

    Create driver.h

---
+++ driver.h
@@ -0,0 +1,28 @@
+/*
+ * driver.h
+ *
+ *  Created on: Sep 22, 2015
+ *      Author: jb
+ *      Description: This program concatenates multiple
+ *      files in a directory into one
+ *      Usage: dircat DIRECTORY... OUTPUT...
+ */
+
+#ifndef DRIVER_H_
+#define DRIVER_H_
+
+// Include libraries
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sstream>
+#include <string>
+
+// Include source files
+#include "utilities.h"
+
+using namespace std;
+
+#endif /* DRIVER_H_ */
