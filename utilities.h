624049e569b2ecdef7a7c239566bcbe45e27d5e3
jonbowring <jonbowring@gmail.com>
2015-10-01 22:19:25 +0200

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,26 @@
+/*
+ * utilities.h
+ *
+ *  Created on: Sep 30, 2015
+ *      Author: jb
+ *      Description: This is a source file for utility functions
+ *      for the dircat program
+ */
+
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <sys/stat.h>
+
+using namespace std;
+
+// Function declarations
+string parseDir(const string iPath);
+string parseFile(const string iPath);
+string fileType(const string iPath);
+
+
+#endif /* UTILITIES_H_ */
